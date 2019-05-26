19ccb16d1d41648fde832d1ecca266ec181f79e2
Francois Drielsma <francois.drielsma@gmail.com>
2019-05-25 22:31:56 -0700

    Changed output name

--- daq.py
+++ daq.py
@@ -37,7 +37,9 @@ temp = Measurement(
 meas.append(temp)
 
 # Initialize the output file
-output_file = "data/{}_{}.csv".format(int(time.time()), OUTPUT_FILE_NAME)
+output_file = "data/{}_{}.csv".format(\
+        time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime()), OUTPUT_FILE_NAME)
+print(output_file)
 output = CSVData(output_file)
 
 # Create dictionary keys
