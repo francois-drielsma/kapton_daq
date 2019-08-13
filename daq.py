c3cf63376a48e9c54667e19373465cd43f92908d
Francois Drielsma <francois.drielsma@gmail.com>
2019-08-13 14:55:48 -0700

    Restructured DAQ as object

--- daq.py
+++ daq.py
@@ -10,178 +10,272 @@ from collections import namedtuple
 from utils.logger import Logger, CSVData
 from utils.virtual_device import VirtualDevice
 
-# Class that handles SIGINT and SIGTERM gracefully
-class Killer:
-    kill_now = False
+class DAQ:
+    """
+    Class that handles data acquisition.
+    """
     def __init__(self):
-        signal.signal(signal.SIGINT, self.exit)
-        signal.signal(signal.SIGTERM, self.exit)
+        """
+        Initialize shared variables.
+        """
+        # Global parameters
+        self._time = 0.         # Data acquisition time
+        self._rate = 0.         # Minimum time between acquisitions
+        self._max_fails = 25    # Maximum allowed number of failed reads
+        self._output_name = ''  # Output file name
+        self._output = None     # Output file
 
-    def exit(self, signum, frame):
-        self.kill_now = True
+        # Parse configuration
+        self._cfg_name = ''     # Configuration file name
+        self._cfg = None        # Configuration dictionary
+        self.parse_config()
 
-# Function that checks that the value returned has the required units
-def convert_units(value, units):
-    if not isinstance(value, pq.Quantity):
-        return pq.Quantity(value, units)
-    if units != value.units:
-        return value.rescale(units)
-    return value
+        # Initialize logger
+        self._logger = None     # Logger
+        self.initialize_logger()
 
-# Function that acquires the requested measurements
-def acquire(probes):
-    readings = []
-    max_fails = 5
-    for i, p in enumerate(probes):
-        # Try to get the measurement, allow for a few consecutive failures
-        fail_count = 0
-        while fail_count < max_fails:
-            try:
-                readings.append(convert_units(p.probe(p.inst, p.meas), p.unit))
-                break
-            except Exception as e:
-                logger.log(str(e), logger.severity.error)
-                logger.log("Failed to read {}, retrying...".format(p.name), logger.severity.error)
-                fail_count += 1
-                if fail_count >= max_fails:
-                    logger.log("Too many consecutive fails, killing DAQ", logger.severity.fatal)
-                    return None
+        # Initialize probes
+        self._probes = []       # Instrument probes
+        self._data_keys = []    # List of measurement keys
+        self.initialize_probes()
 
-    return readings
+    # INNER CLASSES #
 
-# Parse commande line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str,
-                    help="Sets the config file")
-parser.add_argument('--outfile', type=str,
-                    help="Sets the output file name")
-parser.add_argument('--sampling', type=float,
-                    help="Sets the amount of time the DAQ runs for")
-parser.add_argument('--refresh', type=float,
-                    help="Sets the frequency at which the DAQ takes data")
-args = parser.parse_args()
+    class Killer:
+        """
+        Class that handles SIGINT and SIGTERM gracefully.
+        """
+        kill_now = False
+        def __init__(self):
+            signal.signal(signal.SIGINT, self.exit)
+            signal.signal(signal.SIGTERM, self.exit)
 
-# Load the configuration
-cfg_name = 'config/config_default.json'
-if args.config:
-    cfg_name = args.config
+        def exit(self, signum, frame):
+            self.kill_now = True
 
-cfg_file = open(cfg_name)
-cfg = json.load(cfg_file)
+    # METHODS #
 
-# Global parameters
-SAMPLING_TIME = cfg['sampling_time'] if not args.sampling else args.sampling
-REFRESH_RATE = cfg['refresh_rate'] if not args.refresh else args.refresh
-OUTPUT_FILE = 'data/'+cfg['output_name']+'.csv' if not args.outfile else args.outfile
+    @staticmethod
+    def _convert_units(value, units):
+        """
+        Check that the value provided has the required units:
+        - if it is a `float`, give it the right units;
+        - if it is a `Quantity`, rescale to the right units.
+        """
+        if not isinstance(value, pq.Quantity):
+            return pq.Quantity(value, units)
+        if units != value.units:
+            return value.rescale(units)
+        return value
 
-# Initialize the logger
-logger = Logger()
-logger.log("Running DAQ with configuration "+cfg_name)
+    def parse_config(self):
+        """
+        Parse the command line arguments and the configuration file.
+        """
+        # Parse commande line arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--config', type=str,
+                            help="Sets the config file")
+        parser.add_argument('--outfile', type=str,
+                            help="Sets the output file name")
+        parser.add_argument('--sampling', type=float,
+                            help="Sets the amount of time the DAQ runs for")
+        parser.add_argument('--refresh', type=float,
+                            help="Sets the frequency at which the DAQ takes data")
+        args = parser.parse_args()
 
-# Initialize the output file
-open(OUTPUT_FILE, 'w').close()
-output = CSVData(OUTPUT_FILE)
-logger.log("Created DAQ output file "+OUTPUT_FILE)
+        # Load the configuration
+        self._cfg_name = 'config/config_default.json' if not args.config else args.config
+        cfg_file = open(self._cfg_name)
+        self._cfg = json.load(cfg_file)
 
-# Add all the required probes (lambda functions) to the readout chain
-inst_types = ['instrument', 'multimeter', 'power_supply', 'virtual']
-Probe = namedtuple('Probe', 'inst, meas, probe, unit, name')
-probes = []
-for k, i in cfg['instruments'].items():
-    # Set up the instrument
-    logger.log("Setting up instrument {} of type {}".format(k, i['type']))
-    inst = None
-    if i['type'] == 'virtual':
-        inst = VirtualDevice(k)
-    elif i['type'] in inst_types:
-        try:
-            inst = getattr(getattr(ik, i['make']), i['model'])
-        except AttributeError:
-            raise ValueError('Instrument {} not found in InstrumentKit'.format(i['model']))
-    else:
-        raise ValueError('Instrument type not supported: {}'.format(i['type']))
+        # Set global parameters
+        date_str = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime())
+        self._time = self._cfg['sampling_time'] if not args.sampling else args.sampling
+        self._rate = self._cfg['refresh_rate'] if not args.refresh else args.refresh
+        self._output_name = 'data/{}_{}.csv'.format(
+            date_str, self._cfg['output_name']) if not args.outfile else args.outfile
 
-    # Initialize the communication link
-    if i['type'] != 'virtual':
-        logger.log("Initializing communication protocol {}".format(i['comm']))
-        try:
-            inst = getattr(inst, 'open_'+i['comm'])(**i['comm_args'])
-        except AttributeError:
-            raise ValueError('Protocol not supported: {}'.format(i['comm']))
+    def initialize_logger(self):
+        """
+        Initialize a Logger object.
+        """
+        self._logger = Logger()
+        self.log("Setting up DAQ with configuration {}".format(self._cfg_name))
 
-    # Initalize a probe for each measurement
-    for m in i['measurements'].values():
-        logger.log("Setting up {} measurement of name {}".format(m['quantity'], m['name']))
-        unit = getattr(pq, m['unit'])
-        meas, probe = None, None
-        if i['type'] == 'instrument':
-            probe = lambda inst, _: inst.measure()
-        elif i['type'] == 'multimeter':
-            meas = getattr(inst.Mode, m['quantity'])
-            probe = lambda inst, meas: inst.measure(meas)
-        else:
-            if 'channel' in m:
-                inst = inst.channel[m['channel']]
-            meas = inst.__class__.__dict__[m['quantity']]
-            if 'value' in m:
-                logger.log("Setting {} to {} {}".format(m['name'], m['value'],unit.u_symbol))
-                meas.fset(inst, m['value'])
-                time.sleep(0.1)
+    def initialize_probes(self):
+        """
+        Add all the required probes (lambda functions) to the readout chain.
+        """
+        inst_types = ['instrument', 'multimeter', 'power_supply', 'virtual']
+        Probe = namedtuple('Probe', 'inst, meas, probe, unit, name')
+        self._data_keys = ['time']
+        self._probes = []
+        for k, i in self._cfg['instruments'].items():
+            # Set up the instrument
+            self.log("Setting up instrument {} of type {}".format(k, i['type']))
+            inst = None
             if i['type'] == 'virtual':
-                inst.unit = unit
-            probe = lambda inst, meas: meas.fget(inst)
+                inst = VirtualDevice(k)
+            elif i['type'] in inst_types:
+                try:
+                    inst = getattr(getattr(ik, i['make']), i['model'])
+                except AttributeError:
+                    raise ValueError('Instrument {} not found in InstrumentKit'.format(i['model']))
+            else:
+                raise ValueError('Instrument type not supported: {}'.format(i['type']))
 
-        # Append a probe object
-        probes.append(Probe(inst = inst, meas = meas, probe = probe, unit = unit, name = m['name']))
+            # Initialize the communication link
+            if i['type'] != 'virtual':
+                self.log("Initializing communication protocol {}".format(i['comm']))
+                try:
+                    inst = getattr(inst, 'open_'+i['comm'])(**i['comm_args'])
+                except AttributeError:
+                    raise ValueError('Protocol not supported: {}'.format(i['comm']))
 
-# Create dictionary keys
-keys = ['time']
-for m in probes:
-    keys.append('{} [{}]'.format(m.name, m.unit.u_symbol))
-logger.log("DAQ recording keys: ["+",".join(keys)+"]")
+            # Initalize a probe for each measurement
+            for m in i['measurements'].values():
+                self.log("Setting up {} measurement of name {}".format(m['quantity'], m['name']))
+                unit = getattr(pq, m['unit'])
+                meas, probe = None, None
+                if i['type'] == 'instrument':
+                    probe = lambda inst, _: inst.measure()
+                elif i['type'] == 'multimeter':
+                    meas = getattr(inst.Mode, m['quantity'])
+                    probe = lambda inst, meas: inst.measure(meas)
+                else:
+                    if 'channel' in m:
+                        inst = inst.channel[m['channel']]
+                    meas = inst.__class__.__dict__[m['quantity']]
+                    if 'value' in m:
+                        self.log("Setting {} to {} {}".format(m['name'], m['value'],unit.u_symbol))
+                        meas.fset(inst, m['value'])
+                        time.sleep(0.1)
+                    if i['type'] == 'virtual':
+                        inst.unit = unit
+                    else:
+                        inst.output = True
+                    probe = lambda inst, meas: meas.fget(inst)
 
-# Loop for the requested amount of time
-logger.log("Starting DAQ...")
-logger.log("DAQ sampling time: "+(str(SAMPLING_TIME)+' s' if SAMPLING_TIME else 'Unconstrained'))
-logger.log("DAQ refresh rate: "+(str(REFRESH_RATE)+' s' if REFRESH_RATE else 'AFAP'))
-init_time = time.time()
-curr_time = init_time
-ite_count, perc_count, min_count = 0, 0, 0
-killer = Killer()
-while (not SAMPLING_TIME or (curr_time - init_time) < SAMPLING_TIME) and not killer.kill_now:
-    # Acquire measurements
-    readings = acquire(probes)
-    if not readings:
-        break
+                # Append the list of data keys
+                self._data_keys.append('{} [{}]'.format(m['name'], unit.u_symbol))
 
-    # Append the output
-    vals = [time.time()-init_time]
-    vals.extend(readings)
-    output.record(keys, vals)
-    output.write()
-    output.flush()
+                # Append a probe object
+                self._probes.append(Probe(inst, meas, probe, unit, m['name']))
 
-    # Wait for next measurement, if requested
-    time.sleep(REFRESH_RATE)
+    def initialize_output(self):
+        """
+        Initialize output file.
+        """
+        open(self._output_name, 'w').close()
+        self._output = CSVData(self._output_name)
+        self.log("Created DAQ output file {}".format(self._output_name))
+        self.log("DAQ recording keys: [{}]".format(','.join(self._data_keys)))
 
-    # Update, log the progress
-    curr_time = time.time()
-    delta_t = curr_time-init_time
-    ite_count += 1
-    if SAMPLING_TIME and int(10*delta_t/SAMPLING_TIME) > perc_count:
-        ratio = 100*delta_t/SAMPLING_TIME
-        perc_count = int(ratio/10)
-        elapsed_time = str(datetime.timedelta(seconds=int(delta_t)))
-        message = "DAQ running for {} ({:0.0f}%, {} measurements)".\
-                format(elapsed_time, min(ratio, 100), ite_count)
-        logger.log(message)
-    elif not SAMPLING_TIME and int(delta_t/300) > min_count/5:
-        min_count = int(delta_t/60)
-        elapsed_time = str(datetime.timedelta(seconds=int(delta_t)))
-        message = "DAQ running for {} ({} measurements)".format(elapsed_time, ite_count)
-        logger.log(message)
+    def log(self, message, severity=Logger.severity.info):
+        """
+        Logs a message to the standard output location.
+        """
+        self._logger.log(message, severity)
 
-# Close the output file
-logger.log("...DONE!")
-logger.log("Closing DAQ output file")
-output.close()
+    def read(self, probe):
+        """
+        Reads a specific probe.
+        """
+        return self._convert_units(probe.probe(probe.inst, probe.meas), probe.unit)
+
+    def handle_fail(self, probe, error, fail_count):
+        """
+        Handles a failed read from a specific probe.
+        """
+        serr, sfat = Logger.severity.error, Logger.severity.fatal
+        self.log("Failed to read {} {} time(s)".format(probe.name, fail_count), serr)
+        self.log("Got instrument reading error: {}".format(error), serr)
+        self.log("Will retry in one second...", serr)
+        time.sleep(1)
+        if fail_count == self._max_fails:
+            self.log("Too many consecutive fails, killing DAQ...", sfat)
+            return False
+        if fail_count and fail_count % 5 == 0:
+            self.log("Five consecutive fails, reinitializing probes...", serr)
+            self.initialize_probes()
+
+        return True
+
+    def acquire(self):
+        """
+        Probe the list of requested instruments.
+        """
+        readings = []
+        for i, p in enumerate(self._probes):
+            for i in range(self._max_fails):
+                try:
+                    readings.append(self.read(p))
+                    break
+                except Exception as e:
+                    if not self.handle_fail(p, e, i+1):
+                        return None
+
+        return readings
+
+    def run(self):
+        """
+        Main DAQ function.
+        """
+        # Initialize the output file
+        self.initialize_output()
+
+        # Give some context
+        self.log("Starting DAQ...")
+        self.log("DAQ sampling time: "+(str(self._time)+' s' if self._time else 'Unconstrained'))
+        self.log("DAQ refresh rate: "+(str(self._rate)+' s' if self._rate else 'AFAP'))
+
+        # Loop for the requested amount of time
+        init_time = curr_time = time.time()
+        ite_count, perc_count, min_count = 0, 0, 0
+        killer = self.Killer()
+        while (not self._time or (curr_time - init_time) < self._time) and not killer.kill_now:
+            # Acquire measurements, break if None
+            readings = self.acquire()
+            if not readings:
+                break
+
+            # Append the elapsed time
+            curr_time = time.time()
+            delta_t = curr_time-init_time
+            readings = [delta_t]+readings
+
+            # Save to disk
+            self._output.record(self._data_keys, readings)
+            self._output.write()
+            self._output.flush()
+
+            # Wait for next measurement, if minimum time requested
+            time.sleep(self._rate)
+
+            # Update, log the progress
+            ite_count += 1
+            if self._time and int(10*delta_t/self._time) > perc_count:
+                ratio = 100*delta_t/self._time
+                perc_count = int(ratio/10)
+                elapsed_time = str(datetime.timedelta(seconds=int(delta_t)))
+                message = "DAQ running for {} ({:0.0f}%, {} measurements)".\
+                        format(elapsed_time, min(ratio, 100), ite_count)
+                self.log(message)
+            elif not self._time and int(delta_t/300) > min_count/5:
+                min_count = int(delta_t/60)
+                elapsed_time = str(datetime.timedelta(seconds=int(delta_t)))
+                message = "DAQ running for {} ({} measurements)".format(elapsed_time, ite_count)
+                self.log(message)
+
+        # Close the output file
+        self.log("...DONE!")
+        self.log("Closing DAQ output file")
+        self._output.close()
+
+if __name__ == '__main__':
+    # Initialize DAQ
+    daq = DAQ()
+
+    # Run DAQ
+    daq.run()
